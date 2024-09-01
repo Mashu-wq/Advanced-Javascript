23edccd449a31c114c320184616763767b8522aa
Mayesha <mim2007088@stud.kuet.ac.bd>
2024-09-01 15:18:32 +0600

    new challenge added

--- Array/script.js
+++ Array/script.js
@@ -1,10 +1,10 @@
-const currencies = new Map([
-  ["USD", "United States dollar"],
-  ["EUR", "Euro"],
-  ["GBP", "Pound sterling"],
-]);
+// const currencies = new Map([
+//   ["USD", "United States dollar"],
+//   ["EUR", "Euro"],
+//   ["GBP", "Pound sterling"],
+// ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 // Coding Challenge #1
@@ -28,20 +28,91 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
   */
 
 //solution
-const checkDogs = function (dogsJulis, dogsKate) {
-  const dogsJulisCorrected = dogsJulis.slice();
-  dogsJulisCorrected.slice(0, 1);
-  dogsJulisCorrected.slice(-2);
+// const checkDogs = function (dogsJulis, dogsKate) {
+//   const dogsJulisCorrected = dogsJulis.slice();
+//   dogsJulisCorrected.slice(0, 1);
+//   dogsJulisCorrected.slice(-2);
 
-  const dogs = [...dogsJulisCorrected, ...dogsKate];
+//   const dogs = [...dogsJulisCorrected, ...dogsKate];
 
-  dogs.forEach(function (dog, i) {
-    if (dogs >= 3) {
-      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
-    } else {
-      console.log(`Dog number ${i + 1} is still a puppy 🐶`);
-    }
-  });
+//   dogs.forEach(function (dog, i) {
+//     if (dogs >= 3) {
+//       console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+//     } else {
+//       console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+//     }
+//   });
+// };
+
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+
+// const eurToUsd = 1.1;
+// // const movementtoUSD = movements.map(function (mov) {
+// //   return mov * eurToUsd;
+// // });
+
+// const movementToUSD = movements.map((mov) => mov * eurToUsd);
+// console.log(movementtoUSD);
+
+// const movementsDescriptions = movements.map(
+//   (mov, i) =>
+//     `Movement ${i + 1}: You ${mov > 0 ? "deposited" : "withdrew"} ${Math.abs(
+//       mov
+//     )}`
+// );
+// console.log(movementsDescriptions);
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map((age) => (age <= 2 ? 2 * age : 16 + age * 4));
+  const adults = humanAges.filter((age) => age >= 18);
+  console.log(humanAges);
+  console.log(adults);
+  const avgHumanAges =
+    adults.reduce((acc, age) => acc + age, 0) / adults.length;
+  console.log(avgHumanAges);
+  //return avgHumanAges;
 };
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+//console.log(avg1, avg2);
 
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const calcAverageHumanAgeChaining = (ages) =>
+  ages
+    .map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter((age) => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+const newAvg1 = calcAverageHumanAgeChaining([5, 2, 4, 1, 15, 8, 3]);
+const newAvg2 = calcAverageHumanAgeChaining([16, 6, 10, 5, 6, 1, 4]);
+console.log(newAvg1, newAvg2);
